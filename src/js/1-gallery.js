878c30c78a78d48d600cfc455d471550d2f85a4d
Miami <alex.miami.petrov@gmail.com>
2024-06-19 15:13:19 +0300

    m

--- src/js/1-gallery.js
+++ src/js/1-gallery.js
@@ -85,6 +85,9 @@ const galleryItemsMarkup = images
 
 galleryContainer.insertAdjacentHTML('beforeend', galleryItemsMarkup);
 
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
 document.addEventListener('DOMContentLoaded', function () {
   const gallery = new SimpleLightbox('.gallery a', {
     captionsData: 'alt',
